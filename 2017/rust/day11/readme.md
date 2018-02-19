94881972264527bebc3b232c17a39f6424403037
André Vicente Milack <andrevicente.m@gmail.com>
2018-02-19 12:40:50 -0300

    Solve day 11 in Rust

---
+++ 2017/rust/day11/readme.md
@@ -0,0 +1,13 @@
+# Day 11: Hex Ed
+
+## Usage
+
+```shell
+cargo run input.txt
+```
+
+## Test
+
+```shell
+cargo test
+```
---
+++ 2017/rust/day11/src/main.rs
@@ -0,0 +1,103 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::ops::Add;
+use std::ops::AddAssign;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct Position {
+    pub x: isize,
+    pub y: isize,
+}
+
+impl Add for Position {
+    type Output = Position;
+
+    fn add(self, rhs: Position) -> Position {
+        Position {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl AddAssign for Position {
+    fn add_assign(&mut self, other: Position) {
+        *self = *self + other;
+    }
+}
+
+fn analyze_path(input: &str) -> (usize, usize) {
+    let mut pos = Position { x: 0, y: 0 };
+    let mut steps = 0;
+    let mut max_steps = 0;
+
+    let input = input.trim().split(',');
+
+    for step in input {
+        match step {
+            "n" => pos += Position { x: 0, y: 2 },
+            "ne" => pos += Position { x: 1, y: 1 },
+            "se" => pos += Position { x: 1, y: -1 },
+            "s" => pos += Position { x: 0, y: -2 },
+            "sw" => pos += Position { x: -1, y: -1 },
+            "nw" => pos += Position { x: -1, y: 1 },
+            _ => (),
+        };
+
+        steps = calculate_steps(&pos);
+
+        if steps > max_steps {
+            max_steps = steps;
+        }
+    }
+
+    (steps, max_steps)
+}
+
+fn calculate_steps(pos: &Position) -> usize {
+    let abs_x = pos.x.abs() as usize;
+    let abs_y = pos.y.abs() as usize;
+
+    if abs_x >= abs_y {
+        abs_x
+    } else {
+        (abs_x + abs_y) / 2
+    }
+}
+
+fn main() {
+    let arg = env::args().skip(1).next();
+
+    if let None = arg {
+        println!("Usage: day11 <input file>");
+        return;
+    }
+
+    let file = File::open(arg.unwrap());
+
+    if let Err(error) = file {
+        println!("{}", error.to_string());
+        return;
+    }
+
+    let mut input = String::new();
+
+    if let Err(error) = file.unwrap().read_to_string(&mut input) {
+        println!("{}", error.to_string());
+        return;
+    }
+
+    let (steps, max_steps) = analyze_path(input.as_str());
+
+    println!("Fewest steps: {}", steps);
+    println!("Furthest steps: {}", max_steps);
+}
+
+#[test]
+fn test() {
+    assert_eq!(analyze_path("ne,ne,ne"), (3, 3));
+    assert_eq!(analyze_path("ne,ne,sw,sw"), (0, 2));
+    assert_eq!(analyze_path("ne,ne,s,s"), (2, 2));
+    assert_eq!(analyze_path("se,sw,se,sw,sw"), (3, 3));
+}
