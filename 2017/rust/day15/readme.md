02c7632da50a4d85742a7f9ab95c79562144d437
André Vicente Milack <andrevicente.m@gmail.com>
2018-04-14 23:56:56 -0300

    Solve day 15 in Rust

---
+++ 2017/rust/day15/readme.md
@@ -0,0 +1,13 @@
+# Day 15: Dueling Generators
+
+## Usage
+
+```shell
+cargo run <input file>
+```
+
+## Test
+
+```shell
+cargo test
+```
---
+++ 2017/rust/day15/src/main.rs
@@ -0,0 +1,105 @@
+extern crate regex;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::iter;
+use regex::Regex;
+
+fn get_initial_values(input: &str) -> (usize, usize) {
+    let val_regex =
+        Regex::new(r"(?m)^Generator (?P<generator>[A-Z]?) starts with (?P<value>[0-9]+)$").unwrap();
+
+    let mut values: (usize, usize) = (0, 0);
+
+    for line in val_regex.captures_iter(input) {
+        let generator = line.name("generator").unwrap().as_str();
+
+        let value = line.name("value")
+            .unwrap()
+            .as_str()
+            .parse::<usize>()
+            .unwrap();
+
+        match generator {
+            "A" => values.0 = value,
+            "B" => values.1 = value,
+            _ => (),
+        };
+    }
+
+    values
+}
+
+fn calculate_matches(
+    pairs: usize,
+    init_values: (usize, usize),
+    divisors: Option<(usize, usize)>,
+) -> usize {
+    const FACTOR_A: usize = 16_807;
+    const FACTOR_B: usize = 48_271;
+    const MODULO: usize = 2_147_483_647;
+    const MASK: usize = 0xFFFF;
+
+    let get_it = |init, factor, divisor| {
+        iter::repeat(())
+            .scan(init, move |acc, _| {
+                *acc = (*acc * factor) % MODULO;
+                Some(*acc)
+            })
+            .filter(move |n| n % divisor == 0)
+            .take(pairs)
+    };
+
+    let it_a = get_it(init_values.0, FACTOR_A, divisors.unwrap_or((1, 1)).0);
+    let it_b = get_it(init_values.1, FACTOR_B, divisors.unwrap_or((1, 1)).1);
+
+    it_a.zip(it_b)
+        .filter(|&(a, b)| (a & MASK) == (b & MASK))
+        .count()
+}
+
+fn main() {
+    let arg = env::args().skip(1).next();
+
+    if let None = arg {
+        println!("Usage: day15 <input file>");
+        return;
+    }
+
+    let file = File::open(arg.unwrap());
+
+    if let Err(error) = file {
+        println!("{}", error.to_string());
+        return;
+    }
+
+    let mut input = String::new();
+
+    if let Err(error) = file.unwrap().read_to_string(&mut input) {
+        println!("{}", error.to_string());
+        return;
+    }
+
+    let init_values = get_initial_values(input.as_str());
+
+    println!(
+        "No criteria match count: {}",
+        calculate_matches(40_000_000, init_values, None)
+    );
+
+    println!(
+        "Criteria match count: {}",
+        calculate_matches(5_000_000, init_values, Some((4, 8)))
+    );
+}
+
+#[test]
+fn test_no_criteria() {
+    assert_eq!(calculate_matches(40_000_000, (65, 8921), None), 588);
+}
+
+#[test]
+fn test_criteria() {
+    assert_eq!(calculate_matches(5_000_000, (65, 8921), Some((4, 8))), 309);
+}
