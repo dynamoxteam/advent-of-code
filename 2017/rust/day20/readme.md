35c8bffb4fe2e3ddbe72025579726f55b3aaa74c
André Vicente Milack <andrevicente.m@gmail.com>
2018-05-09 18:27:33 -0300

    Solve day 20 part 2 in Rust

--- 2017/rust/day20/readme.md
+++ 2017/rust/day20/readme.md
@@ -29,6 +29,53 @@ lim x[n] ≈ 1/2 * a * n^2
 In the long term, the particle that stays closest to the 0-position is the one
 that has the least absolute acceleration.
 
+## Part 2 solution
+
+```
+xa[n] = xb[n]
+xa[0] + n * va[0] + aa * n * (n + 1) / 2 = xb[0] + n * vb[0] + ab * n * (n + 1) / 2
+(aa - ab) * n * (n + 1) / 2 + (va[0] - vb[0]) * n + (xa[0] - xb[0]) = 0
+
+da = aa - ab
+dv = va[0] - vb[0]
+dx = xa[0] - xb[0]
+```
+
+### For `da` == 0:
+
+```
+dv * n + dx = 0
+n = -dx / dv;
+```
+
+A collision of two particles will happen at the tick `n` if all the following conditions are met:
+
+1. `dx` is divisible by `dv`;
+2. `n` is greater than or equal to zero.
+
+### For `da` != 0:
+
+```
+da * n * (n + 1) / 2 + dv * n + dx = 0
+da/2 * n^2 + (dv + da/2) * n + dx = 0
+
+n = [-(dv + da/2) +- sqrt((dv + da/2)^2 - 2*da*dx)] / da
+n = [-(2*dv + da) +- sqrt((2*dv + da)^2 - 8*da*dx)] / (2 * da)
+
+delta = (2*dv + da)^2 - 8*da*dx
+numerator = -(2*dv + da) +- sqrt(delta)
+denominator = 2 * da
+
+n = numerator / denominator
+```
+
+A collision of two particles will happen at the tick `n` if all the following conditions are met:
+
+1. `delta` is greater than or equal to zero.
+2. `delta` is a perfect square;
+3. `numerator` is divisible by `denominator`;
+4. `n` is greater than or equal to zero.
+
 ## Usage
 
 ```shell
--- 2017/rust/day20/src/main.rs
+++ 2017/rust/day20/src/main.rs
@@ -11,19 +11,67 @@ struct Particle {
     acceleration: (isize, isize, isize),
 }
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Collision {
+    None,
+    Instant(usize),
+    Continuous,
+}
+
 impl Particle {
-    fn load_group(input: &str) -> Vec<Particle> {
-        let mut particles = Vec::new();
+    pub fn load_group(input: &str) -> Vec<Particle> {
+        input
+            .lines()
+            .filter_map(|l| l.parse::<Particle>().ok())
+            .collect()
+    }
 
-        for line in input.lines() {
-            let particle = line.parse::<Particle>();
+    pub fn detect_collision(&self, other: &Particle) -> Option<usize> {
+        let x_collisions = calculate_collisions(
+            self.acceleration.0 - other.acceleration.0,
+            self.velocity.0 - other.velocity.0,
+            self.position.0 - other.position.0,
+        );
 
-            if let Ok(particle) = particle {
-                particles.push(particle)
+        let y_collisions = calculate_collisions(
+            self.acceleration.1 - other.acceleration.1,
+            self.velocity.1 - other.velocity.1,
+            self.position.1 - other.position.1,
+        );
+
+        let z_collisions = calculate_collisions(
+            self.acceleration.2 - other.acceleration.2,
+            self.velocity.2 - other.velocity.2,
+            self.position.2 - other.position.2,
+        );
+
+        for x in x_collisions.iter().cloned() {
+            let collision = x;
+
+            if collision == Collision::None {
+                continue;
+            }
+
+            for y in y_collisions.iter().cloned() {
+                let collision = collision.intersection(&y);
+
+                if collision == Collision::None {
+                    continue;
+                }
+
+                for z in z_collisions.iter().cloned() {
+                    let collision = collision.intersection(&z);
+
+                    match collision {
+                        Collision::Continuous => return Some(0),
+                        Collision::Instant(n) => return Some(n),
+                        Collision::None => continue,
+                    }
+                }
             }
         }
 
-        particles
+        None
     }
 }
 
@@ -50,11 +98,22 @@ impl FromStr for Particle {
     }
 }
 
+impl Collision {
+    fn intersection(&self, other: &Collision) -> Collision {
+        match (*self, *other) {
+            (Collision::Continuous, _) => *other,
+            (_, Collision::Continuous) => *self,
+            (Collision::Instant(is), Collision::Instant(io)) if is == io => *self,
+            _ => Collision::None,
+        }
+    }
+}
+
 fn parse_tuple(input: &str) -> Result<(isize, isize, isize), ()> {
     let mut iter = input
         .trim()
         .split(',')
-        .filter_map(|s| s.parse::<isize>().ok());
+        .filter_map(|s| s.trim().parse::<isize>().ok());
 
     Ok((
         iter.next().ok_or(())?,
@@ -74,6 +133,101 @@ where
         .map(|e| e.0)
 }
 
+fn calculate_collisions(da: isize, dv: isize, dx: isize) -> [Collision; 2] {
+    let mut result = [Collision::None, Collision::None];
+
+    if da != 0 {
+        let delta = (2 * dv + da).pow(2) - 8 * da * dx;
+
+        if delta < 0 {
+            return result;
+        }
+
+        let sqrt_delta = (delta as f64).sqrt() as isize;
+
+        if sqrt_delta.pow(2) != delta {
+            return result;
+        }
+
+        let numerator_base = -(2 * dv + da);
+        let denominator = 2 * da;
+        let mut i = 0;
+
+        for k in [-1, 1].iter().cloned() {
+            let numerator = numerator_base + k * sqrt_delta;
+
+            if numerator % denominator != 0 {
+                continue;
+            }
+
+            let n = numerator / denominator;
+
+            if n < 0 {
+                continue;
+            }
+
+            result[i] = Collision::Instant(n as usize);
+            i += 1;
+        }
+    } else if dv != 0 {
+        if dx % dv == 0 {
+            let n = -dx / dv;
+
+            if n >= 0 {
+                result[0] = Collision::Instant(n as usize);
+            }
+        }
+    } else if dx == 0 {
+        result[0] = Collision::Continuous;
+    }
+
+    result
+}
+
+fn count_colliders(particles: &Vec<Particle>) -> usize {
+    let mut collisions = Vec::new();
+
+    for i1 in 0..particles.len() {
+        for i2 in (i1 + 1)..particles.len() {
+            let n = particles[i1].detect_collision(&particles[i2]);
+
+            if let Some(n) = n {
+                collisions.push((n, i1, i2));
+            }
+        }
+    }
+
+    collisions.sort_by(|c1, c2| c1.0.cmp(&c2.0));
+
+    let mut remove: Vec<bool> = (0..particles.len()).map(|_| false).collect();
+    let mut buffer: Vec<usize> = Vec::new();
+
+    let mut prev_tick = None;
+
+    for collision in collisions {
+        let curr_tick = Some(collision.0);
+
+        if curr_tick != prev_tick {
+            buffer.iter().cloned().for_each(|p| remove[p] = true);
+            buffer.clear();
+        }
+
+        prev_tick = curr_tick;
+
+        if remove[collision.1] || remove[collision.2] {
+            continue;
+        }
+
+        buffer.push(collision.1);
+        buffer.push(collision.2);
+    }
+
+    buffer.iter().cloned().for_each(|p| remove[p] = true);
+    buffer.clear();
+
+    remove.into_iter().filter(|&r| r).count()
+}
+
 fn main() {
     let input = common::load_file_input("day20");
     let particles = Particle::load_group(input.as_str());
@@ -82,14 +236,43 @@ fn main() {
         "Closest to <0, 0, 0>: {}",
         find_closest_to_zero(particles.iter()).unwrap()
     );
+
+    println!(
+        "Non-collider particles: {}",
+        particles.len() - count_colliders(&particles)
+    );
 }
 
 #[test]
-fn test() {
+fn test_closest_to_zero() {
     let input = "p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>\n\
-                 p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>";
+                 p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>\n";
 
     let particles = Particle::load_group(input);
 
     assert_eq!(find_closest_to_zero(particles.iter()), Some(0));
+}
+
+#[test]
+fn test_collisions_const_vel() {
+    let input = "p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>\n\
+                 p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>\n\
+                 p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>\n\
+                 p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>\n";
+
+    let particles = Particle::load_group(input);
+
+    assert_eq!(count_colliders(&particles), 3);
+}
+
+#[test]
+fn test_collisions_const_acc() {
+    let input = "p=< 0, 2,-3>, v=<-1, 1,-3>, a=< 2, 0,-1>\n\
+                 p=<-6, 4, 1>, v=< 7,-4,-5>, a=<-2, 1, 3>\n\
+                 p=<-5, 0, 2>, v=< 0,-2, 2>, a=< 2, 0,-1>\n\
+                 p=<-2,-8,-2>, v=< 2, 8, 4>, a=< 0,-3,-2>\n";
+
+    let particles = Particle::load_group(input);
+
+    assert_eq!(count_colliders(&particles), 2);
 }
